2cb8efecce0271cf06c7f5f8cda36f5f032dee92
AdityaGowari22 <nkadityagowari@gmail.com>
2025-09-04 10:46:22 +0530

    two sum problem optimal solution with unsorted_map...

---
+++ array/optimal/two_sum_sol_2.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int tar = target - nums[i];
+            if(mp.count(tar))
+            {
+                return {mp[tar], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    auto res = s.twoSum(nums, target);
+    for (int i : res) cout << i << " ";
+    return 0;
+}
