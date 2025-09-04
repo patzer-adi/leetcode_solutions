ff6867cce821392e145d29b409720bd70d5b4d3c
AdityaGowari22 <nkadityagowari@gmail.com>
2025-09-04 10:46:06 +0530

    two sum problem optimal solution with map... (sorted)

---
+++ array/optimal/two_sum_sol_1.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int, int> d;
+        for(int i = 0; i < nums.size(); i++)
+        {
+        int tar = target - nums[i];
+            if(d.find(tar) != d.end() )
+            {
+            ans.push_back(d[tar]);
+            ans.push_back(i);
+            break;
+            }
+            d[nums[i]] = i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    auto res = s.twoSum(nums, target);
+    for (int i : res) cout << i << " ";
+    return 0;
+}
