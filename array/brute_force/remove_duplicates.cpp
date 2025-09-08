b2c24f45ca0cd82a68a825a98894f60bee0be2e5
AdityaGowari22 <nkadityagowari@gmail.com>
2025-09-08 10:19:36 +0530

    added 2 solutions

--- array/brute_force/remove_duplicates.cpp
+++ array/brute_force/remove_duplicates.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) 
-    {
-        int i =0;
-        for(int j = 1; j<nums.size(); j++){
-            if(nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
-            }
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
 
+        int k = 1; // index where next unique element should go
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i]; // place unique element
+                k++;
+            }
         }
-        return i+1;
+        // for (int i = 0; i < nums.size(); i++) {
+        //   cout << nums[i];
+        // }
+        return k; // number of unique elements
     }
 };
 
@@ -22,7 +25,7 @@ int main() {
     Solution s;
 
     // Example 1
-    vector<int> nums1 = {1,1,2};
+    vector<int> nums1 = {0,0,1,1,1,2,2,3,3,4};
     int k1 = s.removeDuplicates(nums1);
     cout << "k = " << k1 << ", nums = ";
     for (int i = 0; i < k1; i++) cout << nums1[i] << " ";
