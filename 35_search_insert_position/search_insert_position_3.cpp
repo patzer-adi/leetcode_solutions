f8fd1ef52409bdc4d9f218c65e0c4c91f0d54e62
patzer-adi <nkadityagowari@gmail.com>
2026-06-19 07:15:18 +0530

    one more try

---
+++ 35_search_insert_position/search_insert_position_3.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int high = nums.size() - 1;
+        int low = 0;
+        int mid;
+
+        while(low <= high) {
+            mid = (high + low) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cin >> target;
+
+    Solution obj;
+    cout << obj.searchInsert(nums, target);
+
+    return 0;
+}
